b27b3bbb9eec0b4955db82278d1f332767fc8c62
Colin Beal <colinbealwork@gmail.com>
2018-02-06 14:28:40 +0100

    SQL update

--- wikiventefilm.sql
+++ wikiventefilm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 23, 2018 at 01:04 PM
+-- Generation Time: Feb 06, 2018 at 01:27 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.0.13
 
@@ -43,9 +43,31 @@ CREATE TABLE IF NOT EXISTS `article` (
 
 INSERT INTO `article` (`titre`, `id`, `prix`, `description`, `categorie`, `version`, `urlimage`, `support`) VALUES
 ('Le seigneur des anneaux : la communauté de l''anneau.', 1, 25, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Collector Longue', 'images/lotr1vhs.jpg', 'VHS'),
-('Le seigneur des anneaux : la communauté de l''anneau.', 2, 16, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Classique', '/images/lotr1dvd.png', 'DVD'),
-('Le seigneur des anneaux : la communauté de l''anneau.', 3, 20, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Collector', '/images/lotr1bluray.png', 'BLU-RAY'),
-('Le seigneur des anneaux : la communauté de l''anneau.', 4, 20, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Collector Longue', '/images/lotr1dvd2.png', 'DVD');
+('Le seigneur des anneaux : la communauté de l''anneau.', 2, 16, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Prestige', 'images/lotr1dvd.jpg', 'DVD'),
+('Le seigneur des anneaux : la communauté de l''anneau.', 3, 20, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Collector', 'images/lotr1bluray.jpg', 'BLU-RAY'),
+('Le seigneur des anneaux : la communauté de l''anneau.', 4, 20, 'Le Seigneur des anneaux : La Communauté de l''anneau (The Lord of The Rings: The Fellowship of the Ring) est un film américano-néo-zélandais réalisé par Peter Jackson, sorti en 2001. C''est le premier volet de la trilogie Le Seigneur des anneaux et l''adaptation du livre La Fraternité de l''Anneau de J. R. R. Tolkien.', 'Fantastique', 'Collector Longue', 'images/lotr1dvd2.jpg', 'DVD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meta_menu`
+--
+
+CREATE TABLE IF NOT EXISTS `meta_menu` (
+  `nom` varchar(255) NOT NULL,
+  `lien` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meta_menu`
+--
+
+INSERT INTO `meta_menu` (`nom`, `lien`, `id`) VALUES
+('Accueil', 'accueil.php', 1),
+('Série', 'liste_serie.php', 2),
+('Film', 'liste_film.php', 3),
+('Annonces', 'liste_annonce.php', 4);
 
 --
 -- Indexes for dumped tables
